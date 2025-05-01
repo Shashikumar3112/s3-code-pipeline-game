db495f097d957c29ea04ad182ac068cc7d8dd87e
Shashikumar3112 <69217488+Shashikumar3112@users.noreply.github.com>
2025-05-01 22:42:43 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "my_bucket_region" {
     description = "my default bucket region"
     type = string
-    default = "eu-west-2"
+    default = "us-east-1"
 }
 
 variable "my_bucket_name" {
     description = "my bucket name"
     type = string
-    default = "my-meme-card-game"
+    default = "02terraformstatebucket"
 }
