6a73a3275c66beae82ac18ed8bc04873f2bd68d8
Shashikumar3112 <69217488+Shashikumar3112@users.noreply.github.com>
2025-05-03 20:23:24 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,12 @@
+ # The configuration for the `remote` backend.
+    terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "Shashi3112"
 
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "shashi-terraform-aws"
+        }
+      }
+    }
